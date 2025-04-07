e4e13611bf32df0825b74d7f3eb592068e1bf0dd
Vasily Skorov <vas999-skorov@yandex.ru>
2025-04-07 15:56:04 +0300

    Изменение временной зоны

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
+
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
