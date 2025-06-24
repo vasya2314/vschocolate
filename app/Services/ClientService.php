b16a2b9b8e434f5e626a26b30939d568edd8e574
Vasily Skorov <vas999-skorov@yandex.ru>
2025-06-24 22:47:20 +0300

    FIX обновление клиента

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -2,7 +2,6 @@
 
 namespace App\Services;
 
-use App\DTO\Client\UpdateClientDTO;
 use App\DTO\ClientDTO;
 use App\Models\Client;
 
@@ -22,35 +21,4 @@ class ClientService
 
         return $client;
     }
-
-
-    public function updateClient(Client $client, UpdateClientDTO $updateClientDTO): Client
-    {
-        $data = [];
-
-        if ($updateClientDTO->getName() !== null)
-        {
-            $data['name'] = $updateClientDTO->getName();
-        }
-
-        if ($updateClientDTO->getPhone() !== null)
-        {
-            $data['phone'] = $updateClientDTO->getPhone();
-        }
-
-        if ($updateClientDTO->getFrom() !== null)
-        {
-            $data['from'] = $updateClientDTO->getFrom();
-        }
-
-        if ($updateClientDTO->getComment() !== null)
-        {
-            $data['comment'] = $updateClientDTO->getComment();
-        }
-
-        if (!empty($data)) $client->update($data);
-
-        return $client;
-    }
-
 }
