5ee533105d0f759f11fa2e026d28abf7fa942d1c
Vasily Skorov <vas999-skorov@yandex.ru>
2025-06-25 14:27:53 +0300

    FIX изменение времени

--- routes/console.php
+++ routes/console.php
@@ -10,4 +10,4 @@ use Illuminate\Support\Facades\Schedule;
 //    $this->comment(Inspiring::quote());
 //})->purpose('Display an inspiring quote');
 
-Schedule::command('app:tg-orders-notification')->dailyAt('7:00')->withoutOverlapping(); // Уведомление о заказах
+Schedule::command('app:tg-orders-notification')->dailyAt('8:00')->withoutOverlapping(); // Уведомление о заказах
